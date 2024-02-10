10c4cf63fc946f442953c1d05784ab8b28e035fd
Michael Kasman <mckasman@utexas.edu>
2024-02-09 23:00:52 -0600

    Updated actuation/observation latency & clearance weight

--- src/navigation/path_options.cc
+++ src/navigation/path_options.cc
@@ -167,7 +167,7 @@ vector<navigation::PathOption> samplePathOptions(int num_options,
 float score(float free_path_length, float curvature, float clearance) {
     const float w1 = 1;
     const float w2 = 0;
-    const float w3 = 0;
+    const float w3 = 0.1;
     return w1 * free_path_length + w2 * abs(1/curvature) + w3 * clearance;
 }
 
